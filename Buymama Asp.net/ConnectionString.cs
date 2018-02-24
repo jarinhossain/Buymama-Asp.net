0269c12d9aeaac2e0ede812f59b1bd0848a970a5
robinabdullah <robinabdullah@yahoo.com>
2018-02-25 00:34:43 +0600

    con string update

--- Buymama Asp.net/ConnectionString.cs
+++ Buymama Asp.net/ConnectionString.cs
@@ -7,6 +7,7 @@ namespace Buymama_Asp.net
 {
     public class ConnectionString
     {
-        public static string connectionString =  @"server=DESKTOP-JAP561I\SQLEXPRESS;database=Buymama;integrated security=true";
+        //public static string connectionString =  @"server=DESKTOP-JAP561I\SQLEXPRESS;database=Buymama;integrated security=true";
+        public static string connectionString = @"Server=tcp:buymama.database.windows.net,1433;Initial Catalog=buymama;Persist Security Info=False;User ID=robinabdullah;Password=45659123iN;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
